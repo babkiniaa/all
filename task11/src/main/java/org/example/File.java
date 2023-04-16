c80a55f3a4cbd0caf3e9be162078edfd1a08293f
Ярослав <124196034+babkiniaa@users.noreply.github.com>
2023-04-16 18:32:44 +0300

    Delete task11 directory

--- task11/src/main/java/org/example/File.java
+++
@@ -1,9 +0,0 @@
-package org.example;
-
-import java.io.IOException;
-
-public interface File {
-    public void fileWrite(String fname, String info) throws IOException;
-
-    public String fileRead(String fname);
-}
